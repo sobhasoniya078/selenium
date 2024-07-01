96d2f74bf769b608f458cea9d0c1140c4730d8c2
sobhasoniya078 <sobhasoniya@gmail.com>
2024-07-01 10:27:46 +0530

    Add files via upload

---
+++ com.ust.anootaions/src/test/java/Testcase/Annotations.java
@@ -0,0 +1,66 @@
+package Testcase;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class Annotations {
+  @Test
+  public void first_Method() {
+	  System.out.println("First test");
+  }
+  @Test
+  public void second_Method() {
+	  System.out.println("Second test");
+  }
+  @Test
+  public void third_Method() {
+	  System.out.println("Third test");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("Before method");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("After method");
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("Before class");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("After class");
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("Before test");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("After test");
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+	  System.out.println("before suite");
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+	  System.out.println("After suite");
+  }
+
+}
